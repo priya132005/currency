940271d3a8c7ca9bbe982b96a9454bb44fe87571
priya <priya.kumari811101@gmail.com>
2024-05-20 09:41:40 +0530

    add changes made

--- src/components/inputbox.jsx
+++ src/components/inputbox.jsx
@@ -1,24 +1,27 @@
-import React, { useState } from 'react';
+import React, {useId} from 'react'
 
 function InputBox({
     label,
     amount,
     onAmountChange,
+    onCurrencyChange,
     currencyOptions = [],
     selectCurrency = "usd",
     amountDisable = false,
     currencyDisable = false,
     className = "",
-    onCurrencyChange,
 }) {
-    const amountInputId = useState();
+   const amountInputId = useId()
+
     return (
         <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
             <div className="w-1/2">
-                <label htmlFor={amountInputId} className="inline-block mb-2 text-black/40">{label}</label>
+                <label htmlFor={amountInputId}  className="inline-block mb-2 text-black/40">
+                    {label}
+                </label>
                 <input
                     id={amountInputId}
-                    className="outline-none w-full bg-transparent py-1,5"
+                    className="outline-none w-full bg-transparent py-1.5"
                     type="number"
                     placeholder="Amount"
                     disabled={amountDisable}
@@ -27,18 +30,20 @@ function InputBox({
                 />
             </div>
             <div className="flex flex-wrap justify-end w-1/2 text-right">
-                <p className='w-full mb-2 text-black/40'>currencytype</p>
+                <p className="w-full mb-2 text-black/40">Currency Type</p>
                 <select
-                    className='px-1 bg-gray-100 rounded-lg outline-none cursor-pointer'
+                    className="px-1 py-1 bg-gray-100 rounded-lg outline-none cursor-pointer"
                     value={selectCurrency}
                     onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
                     disabled={currencyDisable}
                 >
-                    {currencyOptions.map((currency) => (
-                        <option key={currency} value={currency}>
+                    
+                        {currencyOptions.map((currency) => (
+                            <option key={currency} value={currency}>
                             {currency}
-                        </option>
-                    ))}
+                            </option>
+                        ))}
+                
                 </select>
             </div>
         </div>
@@ -46,4 +51,3 @@ function InputBox({
 }
 
 export default InputBox;
-
--- src/hooks/usecurrencyinfo.js
+++ src/hooks/usecurrencyinfo.js
@@ -1,14 +1,17 @@
 // useCurrencyInfo.js
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from "react"
 
-function useCurrencyInfo(currency) {
-    const [data, setData] = useState({});
+
+function useCurrencyInfo(currency){
+    const [data, setData] = useState({})
     useEffect(() => {
-        fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json')
-            .then((res) => res.json())
-            .then((res) => setData(res[currency]));
-    }, [currency]);
-    return data;
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        .then((res) => res.json())
+        .then((res) => setData(res[currency]))
+        console.log(data);
+    }, [currency])
+    console.log(data);
+    return data
 }
 
 export default useCurrencyInfo;
